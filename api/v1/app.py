c9b738bb6ffd4efb6b0c0a42bd78ecec8b08934c
Nanakzy <nanakzy@gmail.com>
2024-05-01 12:06:21 -0700

    README.md

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
-"""api"""
 from flask import Flask
 from models import storage
-import os
-from api.v1.views import app_views
-
-app = Flask(__name__)
-app.register_blueprint(app_views)
 
 
-@app.teardown_appcontext
-def teardown(exception):
-    storage.close()
+def create_app():
+    app = Flask(__name__)
+    # ... other configuration
+
+    # Import app_views here
+    from api.v1.views import app_views
+
+    # Register blueprint
+    app.register_blueprint(app_views)
+
+    # ... other app logic
+
+    return app
 
 
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port, threaded=True)
+    app = create_app()
+    app.run(host=os.environ.get('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.environ.get('HBNB_API_PORT', 5000)),
+            threaded=True)
