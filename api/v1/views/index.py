86b8ae3288874ae0a0126ffcf4628799cac206b6
Nanakzy <nanakzy@gmail.com>
2024-05-06 06:38:23 -0700

    index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -22,6 +22,20 @@ def status():
 
 
 @app_views.route('/stats', methods=['GET'])
+def get_stats():
+    """Retrieves the number of each object type"""
+    objects_count = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
+    return jsonify(objects_count)
+
+
+@app_views.route('/stats', methods=['GET'])
 def count():
     '''retrieves the number of each objects by type'''
     count_dict = {}
